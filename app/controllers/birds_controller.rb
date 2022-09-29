687da430242f4116611d62eaec450456fa92df42
chrisphine <onyangoketta@gmail.com>
2022-09-29 16:16:47 +0300

    commit tree 213b1ed261f67af7a364d93e57748e3b18fcaf4a

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -2,6 +2,7 @@ class BirdsController < ApplicationController
 
   # GET /birds
   def index
+    
     birds = Bird.all
     render json: birds
   end
@@ -15,5 +16,11 @@ class BirdsController < ApplicationController
       render json: { error: "Bird not found" }, status: :not_found
     end
   end
+
+def create 
+byebug
+  bird = Bird.create(name:params[:name],species:params[:species])
+  render json: bird, status: :created
+end
 
 end
